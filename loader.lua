ab4d462a0d8a2183f310ba4e383c753a49b2e46e
FlorzyyH4X <105269538+FlorzyyH4X@users.noreply.github.com>
2023-01-26 11:20:34 +1100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local GamesURL = "https://raw.githubusercontent.com/FlorzyyH4X/thinkware/main/games/"
 local PlaceId = game.PlaceId
 
-local ScriptURL = GamesUrl .. PlaceId
+local ScriptURL = GamesUrl .. PlaceId .. ".lua"
 local Loaded = loadstring(game:HttpGet(ScriptURL))
 
 if Loaded then
